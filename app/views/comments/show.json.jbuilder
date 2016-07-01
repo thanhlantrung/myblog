211684a31420c28bdf9745944c158494994efe14
Thanh Trung <thanhlan.trung@gmail.com>
2016-07-01 22:18:26 +1000

    scaffolding

---
+++ app/views/comments/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @comment, :id, :post_id, :body, :text, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :posts
   get 'static_pages/about'
 
   get 'static_pages/contact'
